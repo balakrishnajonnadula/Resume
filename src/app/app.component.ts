b0baaf0649851098b822cc01e07acf7383b10af9
balakrishnajonnadula <myself.jbk@gmail.com>
2021-12-25 19:40:05 +0530

    nav modifcation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,38 @@
-import { Component,ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  encapsulation:ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None
 })
-export class AppComponent {
-  title = 'Resume';
+export class AppComponent implements OnInit {
+
+  
+  @HostListener('window:scroll', ['$event.target'])
+
+  // public title = 'Resume';
+  ngOnInit() {
+
+  }
+
+  onWindowScroll() {
+    let element = document.querySelector('navbar') as HTMLElement;
+    if (window.pageYOffset > element.clientHeight) {
+      element.classList.add('navbar-inverse');
+    } else {
+      element.classList.remove('navbar-inverse');
+    }
+  }
+
+  /*  gotoTop() {
+     window.scroll({
+       top: 0,
+       left: 0,
+       behavior: 'smooth'
+     }); */
 }
+
+
+
